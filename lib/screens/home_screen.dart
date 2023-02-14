b3f928f3b75621eb10ab233f2e09f8c96e84db1d
Amirhossein Jahangiri <amirhosseinjahangiri78@gmail.com>
2023-02-14 18:16:40 +0330

    add provider and shared pref packages, and complete change theme light and dark theme

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app_course/provider/dark_theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SwitchListTile(
+          value: context.watch<DarkThemeProvider>().getDarkTheme,
+          title: Text('Change Theme'),
+          onChanged: (bool value) {
+            context.read<DarkThemeProvider>().setDarkTheme = value;
+          },
+        ),
+      ),
+    );
+  }
+}
